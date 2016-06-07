73a53db366f1278bcaf6f854bcbd9b7acb4c5a8b
Sergey Skrynnikov <minotaar.hh@gmail.com>
2016-06-07 16:26:50 +0300

    included localForage

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ require( './styles/main.scss' );
 
 // inject bundled Elm app into div#main
 var Elm = require( './Main' );
+var localForage = require('localForage');
 Elm.Main.embed( document.getElementById( 'main' ) );
+localForage.setItem('decks', {
+    a: 2,
+    b: 3
+});
